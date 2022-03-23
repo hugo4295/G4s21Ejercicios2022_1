7dad68d12954883b4a9af9668a5486bcff10a9f1
Victor Hugo De la O Martínez <hugo_4295@hotmail.com>
2022-03-22 19:54:47 -0600

    subiendo los archivos faltantes

---
+++ G4s21hugo4295e8/src/Informacion/Datos.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Informacion;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class Datos {
+    String nombre;
+    int edad;
+    String correo;
+
+    public Datos() {
+    }
+
+    public Datos(String nombre, int edad, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
