8e031a500017d75b44a1fae3c283540f86fcd423
Victor Hugo De la O Martínez <hugo_4295@hotmail.com>
2022-04-20 19:21:11 -0500

    codigos faltantes de la practica 13

---
+++ G4s21hugo4295e13/src/g4s21hugo4295e13/G4s21hugo4295e13.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21hugo4295e13;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class G4s21hugo4295e13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFPrincipal jFPrincipal = new JFPrincipal();
+        jFPrincipal.setVisible(true);
+    }
+    
+}
